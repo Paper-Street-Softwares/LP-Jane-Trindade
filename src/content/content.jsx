a0dac3138244b8ead5934bd3f36cac3f6a237827
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-30 09:16:17 -0300

    fix: Link do blog

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -596,7 +596,7 @@ const content = {
       img: imgSteps,
       labelCards: "Ver mais",
       label: "Clique aqui para ver todas as matérias",
-      blogLink: "A_Definir", //sem "https://"
+      blogLink: "drajanetrindadeadv.wordpress.com", //sem "https://"
     },
     faq: {
       miniTag: "TIRE SUAS DÚVIDAS",
