2ed15fde1c7291ff657131165a81cac881c5750e
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-03 17:07:01 -0300

    fix: Hero title

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -114,7 +114,7 @@ export default function Hero({
       </div>
     );
     titleElement = (
-      <div className="text-primary flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7 mb-2">
+      <div className="text-black flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7 mb-2">
         <h1>
           <span
             dangerouslySetInnerHTML={{
